00099f21ad6bc0f21310a6ce67120c4c4db4223e
aman-panwar <aman.panwar2002@gmail.com>
2023-04-15 16:57:59 +0530

    added all basic classes

---
+++ server/Controller/command.py
@@ -0,0 +1,27 @@
+class Command:
+    def execute(self):
+        pass
+
+class Accept:
+    def __init__(self) -> None:
+        pass
+    def execute(self):
+        pass
+    def notify():
+        pass
+
+class Reject:
+    def __init__(self) -> None:
+        pass
+    def execute(self):
+        pass
+    def notify():
+        pass
+
+class Review:
+    def __init__(self) -> None:
+        pass
+    def execute(self):
+        pass
+    def notify():
+        pass
---
+++ server/Model/form.py
@@ -0,0 +1,8 @@
+class Form:
+    def __init__(self) -> None:
+        pass
+    def update(self) -> None:
+        pass
+    def get_form_info(self) ->dict:
+        #returns a json 
+        pass
