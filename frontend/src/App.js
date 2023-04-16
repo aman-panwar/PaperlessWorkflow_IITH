8b1e9139f5f5f25b4a8c15f760a03b9de1c60a27
aman-panwar <aman.panwar2002@gmail.com>
2023-04-16 17:21:45 +0530

    added google auth in react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,48 @@
-import React, {useState, useEffect}  from 'react'
+import React, { useState, useEffect } from 'react'
+import { GoogleLogin } from '@react-oauth/google';
 
-function App(){
+function App() {
   const [data, setData] = useState([{}])
 
   useEffect(() => {
-      fetch("/contri").then(
-        res => res.json()
-      ).then(
-        data=>{
-          setData(data)
-          console.log(data)
-        }
-      )
+    fetch("/contri").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
   }, [])
+
+  const auth_sucess = (msg) => {
+    console.log("login success!")
+    console.log(msg);
+  };
+  const auth_fail = (msg) => {
+    console.log("login fail :(");
+    console.log(msg);
+  };
 
   return (
     <div>
       <h1>project</h1>
-      {(typeof data.project == 'undefined')?(
+      {(typeof data.project == 'undefined') ? (
         <p>loading...</p>
-      ):(
+      ) : (
         <p>{data.project}</p>
       )}
 
-    <h1>contributer</h1>
-      {(typeof data.contributer == 'undefined')?(
+      <h1>contributer</h1>
+      {(typeof data.contributer == 'undefined') ? (
         <p>loading...</p>
-      ):(
+      ) : (
         data.contributer.map((pers, i) => (
-            <p key={i}>{pers}</p>
-          )) 
+          <p key={i}>{pers}</p>
+        ))
       )}
 
+      <GoogleLogin onSuccess={auth_sucess} onError={auth_fail}/>
     </div>
   )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId='112391562895-h6drpui9566iqthca1uc8c43u6u5c0ru.apps.googleusercontent.com'>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
