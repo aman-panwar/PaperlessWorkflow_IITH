245e04c1362757e62c169dcb0b3be364e55a7bb1
Pranav K Nayak <es20btech11035@iith.ac.in>
2023-04-19 12:50:05 +0530

    fixed mimetype error somehow

--- frontend/src/post_request.js
+++ frontend/src/post_request.js
@@ -1,8 +1,4 @@
-import axios from "../node_modules/axios"
-
 document.getElementById('postform').addEventListener('submit', (event) => {
   event.preventDefault();
   const form = new FormData(event.target);
-  const formdata = Object.fromEntries(form);
-  console.log(formdata);
 })
