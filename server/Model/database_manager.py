9f4c6518d2f7c3edf6f77794541834dc375a2cd7
aman-panwar <aman.panwar2002@gmail.com>
2023-04-20 01:37:51 +0530

    added comments and cleaned up code

--- server/Model/database_manager.py
+++ server/Model/database_manager.py
@@ -1,6 +1,3 @@
-""" experimental file to make a singleton resource manager class to handle
-    database client"""
-
 from queue import Queue
 from threading import Lock
 from pymongo import MongoClient
@@ -17,7 +14,15 @@ class SingletonMetaClass(type):
         return cls.my_instances[cls]
 
 class DbManager(metaclass = SingletonMetaClass):
+    """An thread safe singleton object pool class for MongoClient.client
+
+    Intended use:\n
+    with DbManager().get_client() as c:
+        c.get_database('example)\n
+        ... 
+    """
     class ClientWrapper:
+        """a context managing class. sole purpose of this class is to allow DbManager.get_client() to be used in a 'with' statement"""
         def __init__(self, client) -> None:
             self.client = client
         def __enter__(self):
@@ -28,18 +33,37 @@ class DbManager(metaclass = SingletonMetaClass):
             return self.client
 
     def __init__(self) -> None:
-        self.max_size = 10
-        self.clientQ = Queue(maxsize= self.max_size)
+        self.max_size = 10 #total number of clients
+        self.clientQ = Queue(maxsize= self.max_size) #saves the clients available for use
         self.host = 'mongodb+srv://cs20btech11004:Y1JDdwqWBLgOWp2g@cluster0.iz8c5af.mongodb.net/?retryWrites=true&w=majority'
         for _ in range(self.max_size):
             self.clientQ.put(MongoClient(self.host))
         self.lock = Lock()
     def get_client(self):
+        """returns a client wrapper object containing the MongoClient
+
+        Returns:
+            a ClientWrapper obj.
+
+        to get MongoClient obj\n
+        client =  ClientWrapper() or client = ClientWrapper().client
+        """
         while self.clientQ.empty():
             continue
         with self.lock:
             return self.ClientWrapper(self.clientQ.get())
-    def return_client(self, c):
+    def return_client(self, client):
+        """saves the client back into the DbManager"""
         if self.clientQ.full()==False:
             with self.lock:
-                self.clientQ.put(c)
+                self.clientQ.put(client)
+
+
+#############################
+#       intended use        #
+#############################
+# with DbManager().get_client() as c:
+#     db = c.get_database('PaperlessWorkflow')
+#     coll = db.get_collection('Forms')
+#     for x in coll.find({"_id":"643ff5dd326f4d6638bea447"}):
+#         print(x)
