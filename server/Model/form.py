85527925a224f1029eeecc99e470eddb8a35692b
tyagio <cs20btech11060@iith.ac.in>
2023-04-20 15:45:43 +0530

    Abstracted out the form log to data class

--- server/Model/form.py
+++ server/Model/form.py
@@ -1,6 +1,7 @@
-import time
+from data import Data
 from formMetaData import FormMetaData
 from level import Level
+from datetime import date
 # import sys
 # sys.path.insert(1, '/home/ojjas/PaperlessWorkflow_IITH/server/Controller')
 # print(sys.path)
@@ -12,34 +13,22 @@ class Form:
         self.ID=_ID
         self.form_type=_form_type
         self.cur_level=Level(*self.form_type.get_level(0),[],0)#Get 0th layer from metadata
-        self.log=[]
+        self.data=Data()
 
         pass
 
-    def update(self,field_index,u_id,val) -> None:
+    def fill_field(self,field_index,u_id,val) -> None:
         # if field_id not in valid_fields:
         #     return
         field_meta=self.cur_level.get_field_at(field_index)
-        #Later replace by meta classes ,for now dum dum if else
         
         field_entry=fields.FieldFactory(field_meta,val)
-        self.log.append((time.time(),u_id, self.cur_level.get_level_no() ,field_index ,field_entry))
+
+        self.data.append(u_id, self.cur_level.get_level_no() ,field_index ,field_entry)
         # Should be something like Field(type,val)
 
-    def get_form_info(self,till_layer:int) ->dict:
-        ret={}
-        for x in range(till_layer):
-            for y in range(self.form_type.get_field_cnt_at_level(x)):
-                ret[(x,y)]=(0,0)
-        for ele in self.log:
-            kee=(ele[2],ele[3])
-            if  kee in ret :
-                if(ret[kee][0]< ele[0]):
-                    ret[kee]=(ele[0],ele[4])
-            else:
-                ret[kee]=(ele[0],ele[4])
-        print(ret)#IDK why this guy doesnt use the TExtbox print method,probably some inheritance bs
-        return ret
+    def get_form_info(self) ->dict:
+        self.data.get_form_state()
 
     def save_to_db() -> bool:
         pass
@@ -49,10 +38,16 @@ def main():
     
     print(f.form_uid)
     F_instance=Form(0,f)
-    F_instance.update(0,"Pojeshawar","Pojus")
-    F_instance.get_form_info(2)
-    print(F_instance.log[0][-1])
-  
+    F_instance.fill_field(0,"cs20btech11060@iith.ac.in","Ojjas Tyagi")
+    F_instance.fill_field(1,"cs20btech11060@iith.ac.in",date(2023,1,1))
+    F_instance.fill_field(2,"cs20btech11060@iith.ac.in",0)
+    F_instance.get_form_info()
+
+    F_log=F_instance.data.get_log()
+    print(F_log[0][-1])
+    print(F_log[1][-1])
+    print(F_log[2][-1])
+
 if __name__=="__main__":
     main()
 
