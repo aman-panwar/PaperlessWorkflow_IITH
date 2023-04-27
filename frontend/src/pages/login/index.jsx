cfb1957a48fe19c3a1c063c8f23ce2e26ab40847
Pranav K Nayak <es20btech11035@iith.ac.in>
2023-04-27 23:13:59 +0530

    added ID to form context

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -8,73 +8,74 @@ import axios from "axios";
 
 function Login() {
 
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode)
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode)
 
-    const { user, setUser, logoutUser } = useContext(UserContext);
-    const [googleUser, setGoogleUser] = useState(null);
+  const { user, setUser, logoutUser } = useContext(UserContext);
+  const [googleUser, setGoogleUser] = useState(null);
 
-    const login = useGoogleLogin({
-        onSuccess: credentialResponse => setGoogleUser(credentialResponse),
-        onError: err => console.log(err)
-      });
-    
-    useEffect(
-        () => {
-          if (googleUser) {
-            axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${googleUser.access_token}`, {
-              headers: {
-                Authorization: `Bearer ${googleUser.access_token}`,
-                Accept: 'application/json'
-              }
-            })
-            .then(res => {
-                console.log(res.data);
-                setUser(res.data);
-              })
-            .catch(err => console.log(err.data))
+  const login = useGoogleLogin({
+    onSuccess: credentialResponse => setGoogleUser(credentialResponse),
+    onError: err => console.log(err)
+  });
+
+  useEffect(
+    () => {
+      if (googleUser) {
+        axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${googleUser.access_token}`, {
+          headers: {
+            Authorization: `Bearer ${googleUser.access_token}`,
+            Accept: 'application/json'
           }
-        },
-        [ googleUser ]
-    );
-    
-    if (!logoutUser)
-    {
-      logoutUser = () => {
-        googleLogout();
-        setUser(null);
+        })
+          .then(res => {
+            console.log(res.data);
+            setUser(res.data);
+            // TODO: make API call to backend that sends userID, fetches pending forms, sets pendingForms
+            // TODO: get whether admin or not
+          })
+          .catch(err => console.log(err.data))
       }
-    }
-    
-    
+    },
+    [googleUser]
+  );
 
-    return(
-        <>
-        {user ? <Navigate to='/'/> : <></>}
-        <Box 
-            display="flex" 
-            flexDirection="column" 
-            justifyContent="center" 
-            alignItems="center"
-            height="50vh"
-        >
-            <Box>
-                <Typography
-                variant="h1"
-                color={colors.grey[100]}
-                fontWeight="bold"
-                sx ={{ mb: "5px "}}
-                >
-                    Login</Typography>
-            </Box>
-            <br></br>
-            <Box>
-            <button onClick={login}> Sign in with Google </button>
-            </Box> 
+  if (!logoutUser) {
+    logoutUser = () => {
+      googleLogout();
+      setUser(null);
+    }
+  }
+
+
+
+  return (
+    <>
+      {user ? <Navigate to='/' /> : <></>}
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        height="50vh"
+      >
+        <Box>
+          <Typography
+            variant="h1"
+            color={colors.grey[100]}
+            fontWeight="bold"
+            sx={{ mb: "5px " }}
+          >
+            Login</Typography>
         </Box>
-        
-        </>
-    );
+        <br></br>
+        <Box>
+          <button onClick={login}> Sign in with Google </button>
+        </Box>
+      </Box>
+
+    </>
+  );
 }
 
 export default Login;
