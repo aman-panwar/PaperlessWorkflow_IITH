0e2858e982779426845c04c108fdba993d98024f
Pranav K Nayak <es20btech11035@iith.ac.in>
2023-05-01 03:57:28 +0530

    frontend demo endpoint setup

--- frontend/src/pages/form/FillForm.jsx
+++ frontend/src/pages/form/FillForm.jsx
@@ -1,80 +1,94 @@
 // Component that renders the form based on template - APPLICANT
+import axios from 'axios';
 import { useContext, useState } from 'react';
 import { FormContext } from '../../App';
 import { Box, Button } from '@mui/material';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Date from './fields/Date';
 import File from './fields/File';
-import Check  from './fields/Check';
+import Check from './fields/Check';
 import Dropdown from './fields/Dropdown';
 import Text from './fields/Text';
+import { UserContext } from '../../App';
+const baseURL = 'http://localhost:5000';
 
 const FillForm = () => {
 
-    const { formType, setFormType, fillFormInfo, setFillFormInfo } = useContext(FormContext);
-    const isNonMobile = useMediaQuery("(min-width:600px)");
-    const req_fields = fillFormInfo['level_data'][0]['req_fields'];
+  const { formType, setFormType, fillFormInfo, setFillFormInfo } = useContext(FormContext);
+  const { user, setUser } = useContext(UserContext);
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+  const req_fields = fillFormInfo['level_data'][0]['req_fields'];
 
-    const render_elements = () => {
-        const elements = []
-        for(let i=0; i < req_fields.length; i++)
-        {
-            let display_name = req_fields[i][0]
-            let field = req_fields[i][1];
-            let item = <></>;
-            if (field === 'textbox')
-                item = <Text label={display_name}/>
-            else if ( field === 'date')
-                item = <Date/>
-            else if ( field === 'dropdown')
-            {
-                let options = req_fields[i][2];
-                item = <Dropdown label={display_name} options={options}/>
-            }
-            else if ( field === 'file')
-                item = <File label={display_name}/>
-            else if ( field === 'checkbox')
-                item = <Check label={display_name}/>
-            
-            elements.push(item);
-        }
-        return elements;
+  const render_elements = () => {
+    const elements = []
+    for (let i = 0; i < req_fields.length; i++) {
+      let display_name = req_fields[i][0]
+      let field = req_fields[i][1];
+      let item = <></>;
+      if (field === 'textbox')
+        item = <Text label={display_name} name="text" />
+      else if (field === 'date')
+        item = <Date name="date" />
+      else if (field === 'dropdown') {
+        let options = req_fields[i][2];
+        item = <Dropdown label={display_name} options={options} name="dropdown" />
+      }
+      else if (field === 'file')
+        item = <File label={display_name} name="file" />
+      else if (field === 'checkbox')
+        item = <Check label={display_name} name="checkbox" />
+
+      elements.push(item);
     }
+    return elements;
+  }
 
-    const [formData, setFormData] = useState(null);
+  const [formData, setFormData] = useState(null);
 
-    const handleFormSubmit = (event) => {
-        console.log("Submitted Form!");
-        setFillFormInfo(null);
-        setFormType(null);
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submitted Form!");
+    setFillFormInfo(null);
+    setFormType(null);
 
-        // event.preventDefault();
-        // const form = event.target;
-        // const formData = new FormData(form);
-        // const data = Object.fromEntries(formData.entries());
-        // console.log(data);
-    }
+    let formdata = new FormData(event.target);
+    formdata = Object.fromEntries(formdata.entries());
+    console.log(formdata);
+    axios.post(`${baseURL}/demo/submit`, { 'data': formdata, 'user': user })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      })
 
-    return ( 
-        <form onSubmit={handleFormSubmit}>
-            <Box 
-                display="grid" 
-                gap="30px" 
-                gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-                sx = {{
-                    "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
-                }}
-            >
-            {render_elements()}
-            </Box>
-            <Box display="flex" justifyContent="end" mt="20px">
-                <Button type="submit" color="secondary" variant="contained" onClick={handleFormSubmit}>
-                    Submit Form
-                </Button>
-            </Box>
-        </form>
+    // event.preventDefault();
+    // const form = event.target;
+    // const formData = new FormData(form);
+    // const data = Object.fromEntries(formData.entries());
+    // console.log(data);
+  }
 
-    )
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <Box
+        display="grid"
+        gap="30px"
+        gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+        sx={{
+          "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+        }}
+      >
+        {render_elements()}
+      </Box>
+      <Box display="flex" justifyContent="end" mt="20px">
+        <Button type="submit" color="secondary" variant="contained">
+          Submit Form
+        </Button>
+      </Box>
+    </form>
+
+  )
 }
 
 export default FillForm;
--- frontend/src/pages/form/fields/Date.jsx
+++ frontend/src/pages/form/fields/Date.jsx
@@ -1,20 +1,26 @@
+import { FormControl } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers';
 import { useState } from 'react';
 
 const Date = () => {
 
-    const [date, setDate] = useState(null);
-    const changeDate = (newDate) => {
-        setDate(newDate);
-    }
-    return(
-        <DatePicker
-            value ={date}
-            onChange={changeDate}
-            color='secondary'
-            sx ={{ gridColumn: "span 1" }} 
-        />
-    );
+  const [date, setDate] = useState(null);
+  const changeDate = (newDate) => {
+    setDate(newDate);
+    console.log(newDate.format('L'));
+  }
+  return (
+    <FormControl>
+      <DatePicker
+        name='DOB'
+        value={date}
+        onChange={changeDate}
+        color='secondary'
+        sx={{ gridColumn: "span 1" }}
+      />
+
+    </FormControl>
+  );
 }
 
 export default Date;
